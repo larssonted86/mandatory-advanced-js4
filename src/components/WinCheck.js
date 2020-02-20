3649c0bbc962f3c37a4606d0adcd9a3a4bafaedd
Ted Larsson <larssonted86@gmail.com>
2020-02-20 10:32:09 +0100

    first commit

---
+++ src/components/WinCheck.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class WinCheck extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default WinCheck
